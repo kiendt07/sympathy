faf238bfe4d45fad50ed83ed73ebe36c844b994c
Kien Do <kiendo95@gmail.com>
2017-06-07 08:36:32 +0700

    Create a post of a track by uploading new file or using an Zing MP3 url

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,19 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def class_name
+    self.class.name.demodulize
+  end
+
+  def is_track?
+    self.class.name.demodulize == "Track"
+  end
+
+  def is_playlist?
+    self.class.name.demodulize == "Playlist"
+  end
+
+  def is_post?
+    self.class.name.demodulize == "Post"
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,20 @@
 class Post < ApplicationRecord
   belongs_to :user
-  delegate :name, to: :user, prefix: true
+  delegate :name, :email, to: :user, prefix: true
 
-  scope :new_first, ->{order created_at: :desc}
+  scope :new_first, (->{order created_at: :desc})
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :notifications, as: :notificationable
+  belongs_to :content, polymorphic: true
+
+  def original_post
+    post = self
+    post = post.content while post.content.is_post?
+    post
+  end
+
+  def original_content
+    self.original_post.content
+  end
 end
